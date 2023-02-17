2d3c6208a5a97ca76de0f2895b283b533a369dc5
andymc629 <andrewpmcmahon629@gmail.com>
2023-02-17 22:48:50 +0000

    following kubeflow docs quickstart

---
+++ Chapter05/going_with_the_kubeflow/going_with_the_kubeflow/deploy_kubeflow_pipelines.zsh
@@ -0,0 +1,6 @@
+export PIPELINE_VERSION=1.8.5
+kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
+kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
+kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/dev?ref=$PIPELINE_VERSION"
+
+#verify this with port forwarding kubectl port-forward -n kubeflow svc/ml-pipeline-ui 8080:80
