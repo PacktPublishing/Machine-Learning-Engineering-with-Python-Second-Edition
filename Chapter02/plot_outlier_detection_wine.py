1fe207a8c94af9d9854b5aebf541a45ec9d69c7f
andymc629 <andrewpmcmahon629@gmail.com>
2023-02-26 13:58:34 +0000

    chapter 2 python scripts - dummies

---
+++ Chapter02/plot_outlier_detection_wine.py
@@ -0,0 +1,34 @@
+"""
+Outlier detection example for github actions.
+"""
+
+import numpy as np
+from sklearn.covariance import EllipticEnvelope
+from sklearn.svm import OneClassSVM
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+from sklearn.datasets import load_wine
+
+# Define "classifiers" to be used
+classifiers = {
+    "Empirical Covariance": EllipticEnvelope(support_fraction=1.0, contamination=0.25),
+    "Robust Covariance (Minimum Covariance Determinant)": EllipticEnvelope(
+        contamination=0.25
+    ),
+    "OCSVM": OneClassSVM(nu=0.25, gamma=0.35),
+}
+colors = ["m", "g", "b"]
+legend1 = {}
+legend2 = {}
+
+# Get data
+X1 = load_wine()["data"][:, [1, 2]]  # two clusters
+
+# Learn a frontier for outlier detection with several classifiers
+xx1, yy1 = np.meshgrid(np.linspace(0, 6, 500), np.linspace(1, 4.5, 500))
+for i, (clf_name, clf) in enumerate(classifiers.items()):
+    clf.fit(X1)
+    Z1 = clf.decision_function(np.c_[xx1.ravel(), yy1.ravel()])
+    Z1 = Z1.reshape(xx1.shape)
+
+
