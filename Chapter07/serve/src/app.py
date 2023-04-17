6f68e3ee6ec23c4d5cdcf935d8806eb26eaf2f8f
andymc629 <andrewpmcmahon629@gmail.com>
2023-04-17 22:27:24 +0100

    including fastapi-cache2 caching layer, if I hit handlers func twice in a row I overwrite the correct object with a dict .. need to investigate

--- Chapter07/serve/src/app.py
+++ Chapter07/serve/src/app.py
@@ -4,18 +4,71 @@ import datetime
 import pandas as pd
 import pprint
 from helpers.request import ForecastRequest, create_forecast_index
-from registry.mlflow.handler import check_mlflow_health
+from registry.mlflow.handler import MLFlowHandler, check_mlflow_health
 from typing import List
+
+# Caching
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
+from fastapi_cache.decorator import cache
 
 app = FastAPI()
 
+# Start the in memory cache
+@app.on_event("startup")
+async def startup():
+    FastAPICache.init(InMemoryBackend())
+
+@cache(expire=30)  
+async def get_service_handlers():
+    mlflow_handler = MLFlowHandler()
+    return {'mlflow': mlflow_handler}
+    
 @app.get("/health/", status_code=200)
 async def healthcheck():
+    handlers = await get_service_handlers()
     return {
         "serviceStatus": "OK",
-        "modelTrackingHealth": check_mlflow_health(),
+        "modelTrackingHealth": handlers['mlflow'].check_mlflow_health(),
         "modelRegistryHealth": False
         }
+
+@cache(expire=30)
+async def get_models(store_ids: list):
+    handlers = await get_service_handlers()
+    models = []
+    for store_id in store_ids:
+        model = handlers['mlflow'].get_production_model(store_id=store_id)
+        models.append(model)
+    return {'models': models}
+
+@cache(expire=30)
+async def get_model(store_id: str):
+    handlers = await get_service_handlers()
+    model = handlers['mlflow'].get_production_model(store_id=store_id)
+    return {'model': model}
+        
+@app.post("/forecast2/", status_code=200)
+async def parse_request2(forecast_request: List[ForecastRequest]):
+    '''
+    1. Retrieve each model from model registry
+    2. Forecast with it
+    3. Cache it
+    '''
+    #store_ids = []
+    forecasts = []
+    for item in forecast_request:
+        #store_ids.append(item.store_id)
+        model_dict = await get_model(item.store_id)
+        forecast_input = create_forecast_index(
+            begin_date=item.begin_date, 
+            end_date=item.end_date
+            )
+        forecasts.append(model_dict['model'].predict(forecast_input))
+    return forecasts    
+    # # models_dict = await get_models(store_ids=store_ids)
+    # # models = models_dict['models']
+    # return "".join([str(x) for x in models])
 
 
 @app.post("/forecast/", status_code=200)
@@ -29,12 +82,8 @@ async def parse_request(forecast_request: ForecastRequest):
     forecast_request_dict.update({'forecast_index_strings': forecast_index.to_series(keep_tz=False).astype(str).tolist()})
     return forecast_request_dict
 
-@app.post("/forecast2/", status_code=200)
-async def parse_request2(forecast_request: List[ForecastRequest]):
-    models = []
-    store_ids = []
-    for item in forecast_request:
-        store_ids.append(item.store_id)
-    return store_ids
+
+
+
 
 
