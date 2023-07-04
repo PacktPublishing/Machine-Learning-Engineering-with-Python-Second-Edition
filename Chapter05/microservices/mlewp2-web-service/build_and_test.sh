ba76f1cb41df67780880bb28bcd9b3e7c0b55209
andymc629 <andrewpmcmahon629@gmail.com>
2023-07-04 11:03:26 +0100

    ch5 web service sh scripts

--- Chapter05/microservices/mlewp2-web-service/build_and_test.sh
+++ Chapter05/microservices/mlewp2-web-service/build_and_test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 cd app/
-docker build -t basic-ml-webservice:latest .
-#docker run -d -p 5000:5000 basic-ml-webservice:latest
-#curl http://localhost:5000/hello
+# Building on macbook m2
+docker buildx build --platform=linux/amd64 -t basic-ml-microservice:latest .
+# Building on linux
+#docker build -t basic-ml-microservice:latest .
+
+#docker run --rm -it -p 8080:5000 basic-ml-microservice:latest
+#curl http://localhost:8080/forecast
 #curl -d store_number=1 -d forecast_start_date="2021-10-01T00:00:00"
 #docker ps
