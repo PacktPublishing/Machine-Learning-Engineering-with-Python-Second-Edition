7bafbe5ebe2ebb80b14520599cc2b97f9b15f990
andymc629 <andrewpmcmahon629@gmail.com>
2023-07-07 15:53:13 +0100

    policy etc for mwaa

--- Chapter05/mlewp2-airflow/mwaa-example/dags/classification_pipeline_dag.py
+++ Chapter05/mlewp2-airflow/mwaa-example/dags/classification_pipeline_dag.py
@@ -1,7 +1,11 @@
+import datetime
 from datetime import timedelta
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
+
+import logging
+logging.basicConfig(level=logging.INFO)
 
 default_args = {
     'owner': 'Andrew McMahon',
@@ -27,37 +31,75 @@ default_args = {
     # 'trigger_rule': 'all_success'
 }
 
+def get_data():
+    pass
 
-#instantiate DAG
-dag = DAG(
-    'classification_pipeline',
-    default_args=default_args,
-    description="Basic pipeline for classifying the Wine Dataset",
-    schedule_interval=timedelta(days=1),
-)
+def train_model():
+    pass
+
+def persist_model():
+    pass
+
+with DAG(
+    dag_id="classification_pipeline",
+    start_date=datetime.datetime(2021, 10, 1),
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+
+    logging.info("DAG started ...")
+    logging.info("Extracting and clustering data ...")
+    get_data_task = PythonOperator(
+        task_id="get_data",
+        python_callable=get_data
+    )
+    
+    logging.info("Extracting and summarizing data ...")
+    train_model_task = PythonOperator(
+        task_id="train_model",
+        python_callable=train_model
+    )
+    
+    logging.info("Persisting model ...")
+    persist_model_task = PythonOperator(
+        task_id="persist_model",
+        python_callable=persist_model
+    )
+    
+    get_data_task >> train_model_task >> persist_model_task
+    
+    
+    
+# #instantiate DAG
+# dag = DAG(
+#     'classification_pipeline',
+#     default_args=default_args,
+#     description="Basic pipeline for classifying the Wine Dataset",
+#     schedule_interval=timedelta(days=1),
+# )
 
 
-get_data = BashOperator(
-    task_id='get_data',
-    bash_command='python3 /usr/local/airflow/scripts/get_data.py',
-    dag=dag,
-)
+# get_data = BashOperator(
+#     task_id='get_data',
+#     bash_command='python3 /usr/local/airflow/scripts/get_data.py',
+#     dag=dag,
+# )
 
-train_model= BashOperator(
-    task_id='train_model',
-    depends_on_past=False,
-    bash_command='python3 /usr/local/airflow/scripts/train_model.py',
-    retries=3,
-    dag=dag,
-)
+# train_model= BashOperator(
+#     task_id='train_model',
+#     depends_on_past=False,
+#     bash_command='python3 /usr/local/airflow/scripts/train_model.py',
+#     retries=3,
+#     dag=dag,
+# )
 
-# Persist to MLFlow
-persist_model = BashOperator(
-    task_id='persist_model',
-    depends_on_past=False,
-    bash_command='python3 /usr/local/airflow/scripts/persist_model.py',
-    retries=3,
-    dag=dag,
-)
+# # Persist to MLFlow
+# persist_model = BashOperator(
+#     task_id='persist_model',
+#     depends_on_past=False,
+#     bash_command='python3 /usr/local/airflow/scripts/persist_model.py',
+#     retries=3,
+#     dag=dag,
+# )
 
-get_data >> train_model >> persist_model
+# get_data >> train_model >> persist_model
