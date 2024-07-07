4c225d7f9df07db5d55a6f77480b3605348af8a8
andymc629 <andrewpmcmahon629@gmail.com>
2024-07-07 13:44:16 +0100

    Changes in Chaper 3 fix that don't seem in main.

---
+++ Chapter02/mlflow/mlflow_basic.py
@@ -0,0 +1,20 @@
+# Based on mlflow docs at https://www.mlflow.org/docs/latest/quickstart.html
+import os
+from random import random, randint
+from mlflow import log_metric, log_param, log_artifacts
+
+if __name__ == "__main__":
+    # Log a parameter (key-value pair)
+    log_param("param1", randint(0, 100))
+
+    # Log a metric; metrics can be updated throughout the run
+    log_metric("foo", random())
+    log_metric("foo", random() + 1)
+    log_metric("foo", random() + 2)
+
+    # Log an artifact (output file)
+    if not os.path.exists("outputs"):
+        os.makedirs("outputs")
+    with open("outputs/test.txt", "w") as f:
+        f.write("hello world!")
+    log_artifacts("outputs")
---
+++ Chapter07/example_scripts/gpt3-turbo-example.py
@@ -0,0 +1,38 @@
+import os
+from langchain import PromptTemplate, LLMChain
+from langchain.chat_models import ChatOpenAI
+
+# Initiatilise the chat model object
+openai_key = os.getenv('''OPENAI_API_KEY''')
+gpt = ChatOpenAI(model_name='''gpt-3.5-turbo''')
+
+# Create a basic template
+template = """Question: {question}
+
+Answer: """
+
+prompt = PromptTemplate(
+    template=template,
+    input_variables=['question']
+)
+
+# user question
+question = "Where does Andrew McMahon, author of 'Machine Learning Engineering with Python', work?"
+
+# create prompt template > LLM chain
+llm_chain = LLMChain(
+    prompt=prompt,
+    llm=gpt
+)
+
+# ask the user question
+print(llm_chain.run(question))
+
+
+questions = [
+    {'question': "Where does Andrew McMahon, author of 'Machine Learning Engineering with Python', work?"},
+    {'question': "What is MLOps?"},
+    {'question': "What is ML engineering?"},
+    {'question': "What's your favourite flavour of ice cream?"}
+]
+print(llm_chain.generate(questions))
